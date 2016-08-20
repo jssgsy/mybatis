76312abb919bb8ef3bae847f0d8f678163616a2e
jssgsy <fcsnwu@163.com>
2016-08-20 10:24:23 +0800

    补充一些sql语句知识及mybatis增删改方法的返回值说明

--- mybatis/src/test/java/BlogTest.java
+++ mybatis/src/test/java/BlogTest.java
@@ -13,6 +13,16 @@ import java.io.IOException;
 
 /**
  * 对单表的测试,这里以Blog为例,Author类似。
+ *
+ * 1. insert,update,delete方法的返回值均是相应sql语句执行后受影响的记录数。
+ *      insert:返回新增成功的记录数;
+ *      update:返回更新成功的记录数;
+ *      delete:返回删除成功的记录数;
+ *
+ * 2. 关于in操作(删除，查询，更新等):即使in后面的括号中有不匹配的值,in查询也能成功。
+ *      假设数据库中只存在id为1的记录,则delete from blog where id in (1,2,3);也能执行成功。
+ *      结果是成功删除id为1的记录并返回1。即使数据库中没有记录为1,2,3的记录,上述sql语句也能正确执行,返回0.
+ *
  */
 public class BlogTest {
 
@@ -25,7 +35,16 @@ public class BlogTest {
 
         SqlSessionFactory sqlSessionFactory = MybatisSqlSessionUtil.getSqlSessionFactory();
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        sqlSession.insert("BlogMapper.add", new Blog("blogTitle3","blogContent6"));
+        Blog blog = new Blog("blogTitle3", "blogContent6");
+
+        /**
+         * insert返回成功插入的记录数。
+         * 单条记录插入成功返回   1;
+         * 批量插入成功过返回    n;
+         */
+        int affectedRows = sqlSession.insert("BlogMapper.insert", blog);
+        System.out.println("此次insert被影响的记录数为: " + affectedRows);
+        System.out.println("插入时自动产生的id值(需要在sql语句中配置): " + blog.getId());
         sqlSession.commit();
     }
 
@@ -38,7 +57,8 @@ public class BlogTest {
 
         SqlSessionFactory sqlSessionFactory = MybatisSqlSessionUtil.getSqlSessionFactory();
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        sqlSession.update("BlogMapper.update", new Blog((long)7,"xxxx"));
+        int affectedRows = sqlSession.update("BlogMapper.update", new Blog((long)7,"xxxx"));
+        System.out.println("此次update被影响的记录数为: " + affectedRows);
         sqlSession.commit();
 
     }
@@ -65,7 +85,8 @@ public class BlogTest {
     public void deleteBlogById() throws IOException{
         SqlSessionFactory sqlSessionFactory = MybatisSqlSessionUtil.getSqlSessionFactory();
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        sqlSession.delete("BlogMapper.deleteById", 7);
+        int affectedRows = sqlSession.delete("BlogMapper.deleteById", 7);
+        System.out.println("此次delete被影响的记录数为: " + affectedRows);
         sqlSession.commit();
     }
 
